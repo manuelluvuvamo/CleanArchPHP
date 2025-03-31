4eedbb334bd6c14c05be7fcb06f57d3432b9cab8
manuelluvuvamo <manuelluvuvamo337@gmail.com>
2025-03-31 14:27:07 +0100

    Melhoria mensagem de evento de aluno matriculado

--- src/Academico/Dominio/Aluno/LogDeAlunoMatriculado.php
+++ src/Academico/Dominio/Aluno/LogDeAlunoMatriculado.php
@@ -12,7 +12,7 @@ class LogDeAlunoMatriculado extends OuvinteDeEvento
      */
     public function reageAo (Evento $alunoMatriculado): void
     {
-        fprintf(STDERR, 'Aluno com CPF %s foi matriculado na data %s', (string) $alunoMatriculado->cpfAluno(), $alunoMatriculado->momento()->format('d-m-Y'));
+        fprintf(STDERR, 'Aluno com CPF %s foi matriculado na data %s ', (string) $alunoMatriculado->cpfAluno(), $alunoMatriculado->momento()->format('d-m-Y'));
     }
 
     public function sabeProcessar (Evento $evento): bool
